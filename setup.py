bcc8b1cb22c6d7ebeedda19ec287af8d07bd25fc
Carl Voller <27472988+carlvoller@users.noreply.github.com>
2025-03-08 10:58:04 +0000

    Test using cwd to infer overlay path

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
-from sysconfig import get_path
+from sysconfig import get_path, get_python_version
 from glob import glob
+import os
+
+python_version = get_python_version()
+cwd = os.getcwd()
 
 platlib = get_path("platlib")
+platlib_tmp_overlay = f"{cwd}/overlay/lib/python{python_version}/site-packages"
+
+libcuml_path = f"{platlib}/libcuml" if os.path.isdir(f"{platlib}/libcuml") else f"{platlib_tmp_overlay}/libcuml"
+libraft_path = f"{platlib}/libraft" if os.path.isdir(f"{platlib}/libraft") else f"{platlib_tmp_overlay}/libraft"
+
+
 
 __version__ = "0.0.2"
 
@@ -14,12 +24,12 @@ ext_modules = [
         language="c++",
         extra_compile_args=["-O3", "-std=c++17", "-g"],
         include_dirs=[
-          f"{platlib}/libcuml/include",
-          f"{platlib}/libraft/include",
+          f"{libcuml_path}/include",
+          f"{libraft_path}/include",
         ],
         libraries=["cuml++"],
-        library_dirs=[f"{platlib}/libcuml/lib64"],
-        runtime_library_dirs=[f"{platlib}/libcuml/lib64"],
+        library_dirs=[f"{libcuml_path}/lib64"],
+        runtime_library_dirs=[f"{libcuml_path}/lib64"],
         define_macros=[("VERSION_INFO", __version__)],
     ),
 ]
